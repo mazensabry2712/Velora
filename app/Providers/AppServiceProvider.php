ebf23722107ee3c6983aa78dc4e1190daa07b777
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-27 16:39:02 +0300

    Register domain ports and infrastructure adapters

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Application\Shared\Contracts\TransactionManager;
+use App\Domain\Pricing\Contracts\CountryPriceSelector;
 use App\Domain\Queue\Contracts\QueueRepository as DomainQueueRepository;
+use App\Domain\Reporting\Contracts\ReportReader;
 use App\Domain\Shared\Contracts\PaymentGatewayResolver;
 use App\Domain\Subscription\Contracts\SubscriptionReader;
+use App\Domain\Tenant\Contracts\TenantRegistrar;
 use App\Infrastructure\Billing\LegacySubscriptionReader;
 use App\Infrastructure\Persistence\LaravelTransactionManager;
+use App\Infrastructure\Pricing\LegacyCountryPriceSelector;
+use App\Infrastructure\Reporting\LegacyReportReader;
+use App\Infrastructure\Tenancy\LegacyTenantRegistrar;
 use App\Models\Appointment;
 use App\Models\SystemSetting;
 use App\Observers\AppointmentObserver;
@@ -25,11 +31,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(PaymentGatewayManager::class);
-
         $this->app->bind(TransactionManager::class, LaravelTransactionManager::class);
         $this->app->bind(PaymentGatewayResolver::class, PaymentGatewayRouter::class);
         $this->app->bind(SubscriptionReader::class, LegacySubscriptionReader::class);
         $this->app->bind(DomainQueueRepository::class, QueueRepository::class);
+        $this->app->bind(ReportReader::class, LegacyReportReader::class);
+        $this->app->bind(CountryPriceSelector::class, LegacyCountryPriceSelector::class);
+        $this->app->bind(TenantRegistrar::class, LegacyTenantRegistrar::class);
     }
 
     public function boot(): void
@@ -38,15 +46,15 @@ class AppServiceProvider extends ServiceProvider
 
         View::composer('layouts.landing', function ($view) {
             try {
-                $appName             = SystemSetting::get('app_name', config('app.name', 'Velora'));
-                $appLogoUrl          = SystemSetting::get('app_logo_url', '');
+                $appName = SystemSetting::get('app_name', config('app.name', 'Velora'));
+                $appLogoUrl = SystemSetting::get('app_logo_url', '');
                 $registrationEnabled = SystemSetting::get('registration_enabled', true);
-                $defaultTrialDays    = SystemSetting::get('default_trial_days', 14);
+                $defaultTrialDays = SystemSetting::get('default_trial_days', 14);
             } catch (\Throwable $e) {
-                $appName             = config('app.name', 'Velora');
-                $appLogoUrl          = '';
+                $appName = config('app.name', 'Velora');
+                $appLogoUrl = '';
                 $registrationEnabled = true;
-                $defaultTrialDays    = 14;
+                $defaultTrialDays = 14;
             }
 
             $view->with(compact('appName', 'appLogoUrl', 'registrationEnabled', 'defaultTrialDays'));
