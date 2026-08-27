3811439b12627c6860bcad0d342dd9e1ea43c225
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-27 16:55:38 +0300

    refactor: bind Moyasar webhook boundary

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ namespace App\Providers;
 use App\Application\Shared\Contracts\TransactionManager;
 use App\Application\Subscription\Events\SubscriptionUpgradeRequested;
 use App\Domain\Administration\Contracts\SystemNotificationReader;
+use App\Domain\Billing\Contracts\MoyasarWebhookProcessor as MoyasarWebhookProcessorContract;
 use App\Domain\Billing\Contracts\StripeWebhookProcessor;
 use App\Domain\Landing\Contracts\LandingSettingsReader;
 use App\Domain\Pricing\Contracts\CountryPriceSelector;
@@ -20,6 +21,7 @@ use App\Infrastructure\Administration\LegacySystemNotificationReader;
 use App\Infrastructure\Billing\EloquentUpgradeRequestWriter;
 use App\Infrastructure\Billing\LegacySubscriptionReader;
 use App\Infrastructure\Landing\LegacyLandingSettingsReader;
+use App\Infrastructure\Payments\Moyasar\MoyasarWebhookProcessor;
 use App\Infrastructure\Payments\Stripe\StripeWebhookProcessor as StripeWebhookProcessorImplementation;
 use App\Infrastructure\Persistence\LaravelTransactionManager;
 use App\Infrastructure\Pricing\LegacyCountryPriceSelector;
@@ -53,6 +55,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(SystemNotificationReader::class, LegacySystemNotificationReader::class);
         $this->app->bind(LandingSettingsReader::class, LegacyLandingSettingsReader::class);
         $this->app->bind(StripeWebhookProcessor::class, StripeWebhookProcessorImplementation::class);
+        $this->app->bind(MoyasarWebhookProcessorContract::class, MoyasarWebhookProcessor::class);
     }
 
     public function boot(): void
