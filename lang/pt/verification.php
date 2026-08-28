7c43f5713614f4400fff57ea322153ae469f0b4c
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 01:22:54 +0300

    i18n: add Portuguese verification bundle

---
+++ lang/pt/verification.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'E-mail verificado',
+    'email_verified' => 'E-mail verificado',
+    'message' => 'O seu endereço de e-mail foi verificado com sucesso. Volte à página de configuração do seu espaço de trabalho para continuar.',
+    'business' => 'Negócio',
+];
