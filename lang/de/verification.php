36550f8f7776e121ab4e8b312b07a309cbd0bd82
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 01:22:44 +0300

    i18n: add German verification bundle

---
+++ lang/de/verification.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'E-Mail bestätigt',
+    'email_verified' => 'E-Mail bestätigt',
+    'message' => 'Ihre E-Mail-Adresse wurde erfolgreich bestätigt. Kehren Sie zur Einrichtung Ihres Arbeitsbereichs zurück, um fortzufahren.',
+    'business' => 'Unternehmen',
+];
