d2c9174e076c744b3fc1c354038380af82fa75d1
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 01:31:37 +0300

    i18n: add English password recovery strings

---
+++ lang/en/password_reset.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'title' => 'Password recovery',
+    'heading' => 'Forgot your password?',
+    'description' => 'Enter your email and we will send a secure reset link.',
+    'send_link' => 'Send reset link',
+    'back_to_login' => 'Back to sign in',
+    'secure_recovery' => 'Secure account recovery',
+    'reset_title' => 'Reset your password',
+    'reset_heading' => 'Choose a new password',
+    'reset_description' => 'Create a new password for your Velora workspace account.',
+    'confirm_password' => 'Confirm new password',
+    'update_password' => 'Update password',
+    'token_note' => 'This reset link expires after 60 minutes and can only be used once.',
+    'sent' => 'If the account exists, a password reset link has been sent to the email address.',
+    'reset_success' => 'Your password has been reset. Please sign in again.',
+    'email_subject' => 'Reset your Velora password',
+];
