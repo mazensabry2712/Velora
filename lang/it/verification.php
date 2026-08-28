dfbcf2cdef4983869ad4817e4ef9fa7dab5ffc3f
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 01:22:49 +0300

    i18n: add Italian verification bundle

---
+++ lang/it/verification.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Email verificata',
+    'email_verified' => 'Email verificata',
+    'message' => 'Il tuo indirizzo email è stato verificato con successo. Torna alla pagina di configurazione del tuo spazio di lavoro per continuare.',
+    'business' => 'Attività',
+];
