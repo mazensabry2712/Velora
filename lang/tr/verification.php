e96940f0af847fb91d3f85edc9b5baca30acb937
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 01:23:13 +0300

    i18n: add Turkish verification bundle

---
+++ lang/tr/verification.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'E-posta doğrulandı',
+    'email_verified' => 'E-posta doğrulandı',
+    'message' => 'E-posta adresiniz başarıyla doğrulandı. Devam etmek için çalışma alanı kurulum sayfanıza dönün.',
+    'business' => 'İşletme',
+];
