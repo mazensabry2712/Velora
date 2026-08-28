e282f8544bb8c39f5d619b80a883d7520d751440
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 01:23:25 +0300

    i18n: add Dutch verification bundle

---
+++ lang/nl/verification.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'E-mail geverifieerd',
+    'email_verified' => 'E-mail geverifieerd',
+    'message' => 'Je e-mailadres is succesvol geverifieerd. Ga terug naar de instellingenpagina van je werkruimte om verder te gaan.',
+    'business' => 'Bedrijf',
+];
