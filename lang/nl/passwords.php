d513673a54fb99d112318a2da5ac42977b52ed89
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-28 22:45:32 +0300

    i18n: add Dutch password reset translations

---
+++ lang/nl/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset' => 'Je wachtwoord is opnieuw ingesteld.',
+    'sent' => 'We hebben je per e-mail een link gestuurd om je wachtwoord opnieuw in te stellen.',
+    'throttled' => 'Wacht even voordat je het opnieuw probeert.',
+    'token' => 'Deze token voor het opnieuw instellen van je wachtwoord is ongeldig.',
+    'user' => 'We kunnen geen gebruiker met dat e-mailadres vinden.',
+];
