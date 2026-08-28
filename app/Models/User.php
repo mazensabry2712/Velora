f5e923a786187a953416e3fd370ba3cc944b24ab
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-28 22:18:03 +0300

    feat: add persistent locale preference to tenant users

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'locale',
         'phone',
         'specialization',
         'specialization_ar',
