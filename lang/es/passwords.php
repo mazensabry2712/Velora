19a6a4a59bb380919354f5a5e965d03faa4b5180
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-28 22:44:54 +0300

    i18n: add Spanish password reset translations

---
+++ lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Te hemos enviado por correo el enlace para restablecer tu contraseña.',
+    'throttled' => 'Espera antes de volver a intentarlo.',
+    'token' => 'Este token para restablecer la contraseña no es válido.',
+    'user' => 'No encontramos un usuario con esa dirección de correo electrónico.',
+];
