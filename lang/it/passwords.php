0995ed23f54ae1e967a7bfad4251b0e6622d0426
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-28 22:44:59 +0300

    i18n: add Italian password reset translations

---
+++ lang/it/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset' => 'La tua password è stata reimpostata.',
+    'sent' => 'Ti abbiamo inviato via email il link per reimpostare la password.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Il token per la reimpostazione della password non è valido.',
+    'user' => 'Non troviamo un utente con questo indirizzo email.',
+];
