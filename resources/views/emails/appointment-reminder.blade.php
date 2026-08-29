7f8bfa130f075f797d72c9f6e0894485d1c24603
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-29 19:16:26 +0300

    fix: point appointment reminder email to public tracking details

--- resources/views/emails/appointment-reminder.blade.php
+++ resources/views/emails/appointment-reminder.blade.php
@@ -1,19 +1,25 @@
 @component('mail::message')
 # {{ __('notifications.appointment_reminder.subject', [], $locale) }}
 
-{{ __('notifications.appointment_reminder.greeting', ['name' => $user->name], $locale) }}
+{{ __('notifications.appointment_reminder.greeting', ['name' => $customerName], $locale) }}
 
 {{ __('notifications.appointment_reminder.message', [], $locale) }}
 
 ## {{ __('notifications.appointment_reminder.details', [], $locale) }}
 
-**{{ __('notifications.appointment_reminder.date', ['date' => $appointment->date->format('Y-m-d')], $locale) }}**
-**{{ __('notifications.appointment_reminder.time', ['time' => $appointment->time_slot], $locale) }}**
-**{{ __('notifications.appointment_reminder.tenant', ['tenant' => tenant()->name], $locale) }}**
+**{{ __('notifications.appointment_reminder.date', ['date' => $appointmentDate], $locale) }}**  
+**{{ __('notifications.appointment_reminder.time', ['time' => $appointmentTime], $locale) }}**  
+**{{ __('notifications.appointment_reminder.service', ['service' => $serviceName], $locale) }}**  
+**{{ __('notifications.appointment_reminder.staff', ['staff' => $staffName], $locale) }}**  
+**{{ __('notifications.appointment_reminder.tenant', ['tenant' => tenant()->name], $locale) }}**  
+**{{ __('notifications.appointment_reminder.queue', ['number' => $queueNumber], $locale) }}**  
+**{{ __('notifications.appointment_reminder.reference', ['reference' => $reference], $locale) }}**
 
-@component('mail::button', ['url' => config('app.url')])
+@component('mail::button', ['url' => $trackingUrl])
 {{ __('notifications.view_details', [], $locale) }}
 @endcomponent
+
+{{ __('notifications.appointment_reminder.tracking', ['url' => $trackingUrl], $locale) }}
 
 {{ __('notifications.appointment_reminder.footer', [], $locale) }}
 
